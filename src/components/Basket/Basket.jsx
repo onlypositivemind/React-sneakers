476e084f35df7a130d5f1de3e683c17f35b4bff4
Evgenii <evgenypetrov5230@gmail.com>
2022-12-01 19:59:22 +0300

    feat: opening and closing basket

--- src/components/Basket/Basket.jsx
+++ src/components/Basket/Basket.jsx
@@ -2,13 +2,23 @@ import BasketCard from '../../components/BasketCard/BasketCard';
 import Button from '../../components/Button/Button';
 import s from './Basket.module.scss';
 
-const Basket = () => {
-	return (
-		<div style={{ display: 'none' }} className={s.overlay}>
-			<div className={s.drawer}>
+const Basket = ({ isOpen, basketCloseHandler }) => {
+	
+	const stopPropagation = event => {
+		event.stopPropagation();
+	};
+	
+	return isOpen && (
+		<div className={s.overlay} onClick={basketCloseHandler}>
+			<div className={s.drawer} onClick={stopPropagation}>
 				<div className={s.top}>
 					<p className={s.title}>Корзина</p>
-					<button className={s.close}>&#128939;</button>
+					<button
+						className={s.close}
+						onClick={basketCloseHandler}
+					>
+						&#128939;
+					</button>
 				</div>
 				<div className={s.cardsWrapper}>
 					<BasketCard />
--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,4 +1,5 @@
 import { NavLink, Outlet } from 'react-router-dom';
+import { useState } from 'react';
 import Basket from '../Basket/Basket';
 import Logo from '../../shared/images/logo.png';
 import BasketSVG from '../../shared/images/icons/basket.svg';
@@ -7,10 +8,19 @@ import UserSVG from '../../shared/images/icons/user.svg';
 import s from './Layout.module.scss';
 
 const Layout = () => {
+	
+	const [isOpen, setIsOpen] = useState(false);
+	const basketOpenHandler = () => {
+		setIsOpen(true);
+	};
+	const basketCloseHandler = () => {
+		setIsOpen(false);
+	};
+	
 	return (
 		<div className={s.wrapper}>
 			
-			<Basket />
+			<Basket isOpen={isOpen} basketCloseHandler={basketCloseHandler} />
 			
 			<header className={s.header}>
 				
@@ -25,7 +35,7 @@ const Layout = () => {
 				</div>
 				
 				<ul className={s.headerRight}>
-					<li>
+					<li onClick={basketOpenHandler}>
 						<img src={BasketSVG} alt="Basket" />
 						<span className={s.count}>1205 руб.</span>
 					</li>
